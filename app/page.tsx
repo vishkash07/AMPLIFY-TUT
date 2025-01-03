8c0fa2ed8032eb49f21fe0ff2e370ee8c1c2b9fc
Vishal <42697412+vishkash07@users.noreply.github.com>
2025-01-03 20:22:45 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@
 import { useState, useEffect } from "react";
 import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
+import { useAuthenticator } from "@aws-amplify/ui-react";
 import "./../app/app.css";
 import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
@@ -13,6 +14,8 @@ Amplify.configure(outputs);
 const client = generateClient<Schema>();
 
 export default function App() {
+    
+  const { signOut } = useAuthenticator();
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
   function listTodos() {
@@ -20,7 +23,10 @@ export default function App() {
       next: (data) => setTodos([...data.items]),
     });
   }
-
+  
+  function deleteTodo(id: string) {
+    client.models.Todo.delete({ id })
+  }
   useEffect(() => {
     listTodos();
   }, []);
@@ -37,7 +43,7 @@ export default function App() {
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
+          <li onClick={() => deleteTodo(todo.id)} key={todo.id}>{todo.content}</li>
         ))}
       </ul>
       <div>
@@ -47,6 +53,8 @@ export default function App() {
           Review next steps of this tutorial.
         </a>
       </div>
+      <button onClick={signOut}>Sign out</button>
     </main>
+    
   );
 }
